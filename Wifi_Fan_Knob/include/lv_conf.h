4c197706964b848a21830185fd8b214ce24e9789
Brad Hedges <sergeant82d@gmail.com>
2026-09-22 17:41:27 -0500

    Ini file updates before beginning

---
+++ Wifi_Fan_Knob/include/lv_conf.h
@@ -0,0 +1,10 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_HOR_RES_MAX 240
+#define LV_VER_RES_MAX 240
+#define LV_COLOR_DEPTH 16
+#define LV_USE_PERF_MONITOR 0
+#define LV_USE_MEM_MONITOR 0
+
+#endif
