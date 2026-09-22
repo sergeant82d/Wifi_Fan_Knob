3b0fa97c5476b9864bcd2422e4543d431a2de0b2
Brad Hedges <sergeant82d@gmail.com>
2026-09-22 18:48:15 -0500

    added stub webserver code, compiles

---
+++ Wifi_Fan_Knob/include/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+void loadConfig();
+void saveConfig();
+void setDefaultConfig();
+
+#endif
---
+++ Wifi_Fan_Knob/src/config.cpp
@@ -0,0 +1,13 @@
+#include "config.h"
+
+void loadConfig() {
+  // TODO: Load from SPIFFS
+}
+
+void saveConfig() {
+  // TODO: Save to SPIFFS
+}
+
+void setDefaultConfig() {
+  // TODO: Reset to defaults
+}
